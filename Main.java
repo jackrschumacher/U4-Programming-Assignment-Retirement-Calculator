e850781ee51b34658a8db09e89952c554e6916da
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-11 16:38:28 +0000

    Start inputs

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
-import java.util.Scanner();
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner input = new Scanner(System.in);
+    System.out.println("=====Retirement Savings Calculator=====");
+    System.out.println("Please enter your savings per paycheck.");
+    double savingsPerPaycheck = input.nextDouble();
+    System.out.println("Your savings per paycheck is : "+savingsPerPaycheck);
+    
+    
+    
   }
 }
