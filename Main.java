f61de1ac657ae3348cad7d37ad4d52d1e4458b9c
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-11 16:40:06 +0000

    Add Comments

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 
+// Import Scanner
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+    // Set up scanner
     System.out.println("=====Retirement Savings Calculator=====");
+    // Ask user to enter their savings amount from thier paycheck. Prints out to the console the amount they entered.
     System.out.println("Please enter your savings per paycheck.");
     double savingsPerPaycheck = input.nextDouble();
     System.out.println("Your savings per paycheck is : "+savingsPerPaycheck);
+    System.out.println("")
     
     
     
