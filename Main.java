fb68917c9e514d7932080233d55e5ed5c2691243
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-11 18:49:48 +0000

    Remove return value

--- Main.java
+++ Main.java
@@ -6,20 +6,20 @@ class Main {
     Scanner input = new Scanner(System.in);
     // Set up scanner
     System.out.println("=====Retirement Savings Calculator=====");
-    // Ask user to enter their savings amount from thier paycheck. Prints out to the console the amount they entered.
+    // Ask user to enter their savings amount from thier paycheck.
     System.out.println("Please enter your savings per paycheck.");
     double savingsPerPaycheck = input.nextDouble();
-    System.out.println("Your savings per paycheck is : "+savingsPerPaycheck);
     // Ask user to enter the number of paychecks that they receive per year
     System.out.println("========================================");
     System.out.println("How many paychecks do you recive per year?");
     int paychecksPerYear = input.nextInt();
-    System.out.println("You usually receive: "+paychecksPerYear+" paychecks per year");
     System.out.println("========================================");
     System.out.println("What is your expected return on investment (a percentage)");
     double returnOnInvestment = input.nextDouble();
-    System.out.println("Your expected return on investment is: "+ returnOnInvestment + "percent.");
     System.out.println("========================================");
+    System.out.println("How many years would you like to run the calculation for?");
+    int yearsToRun = input.nextInt();
+    
     
     
     
