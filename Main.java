48eaee9e17650e55583f8616a01dbe9e0d970823
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-13 16:49:39 +0000

    Comments

--- Main.java
+++ Main.java
@@ -13,23 +13,30 @@ class Main {
     System.out.println("========================================");
     System.out.println("How many paychecks do you recive per year?");
     double paychecksPerYear = input.nextInt();
+    // Ask user to input return on investment
     System.out.println("========================================");
     System.out.println("What is your expected return on investment (a percentage as a decimal)");
     double returnOnInvestment = input.nextDouble();
+    // Ask how many years the user would like the program to calculate for
     System.out.println("========================================");
     System.out.println("How many years would you like to run the calculation for?");
     int yearsToRun = input.nextInt();
+    // Ask the user how big thier current account is
     System.out.println("========================================");
     System.out.println("What is the current size of your account?");
     double accountSize = input.nextDouble();
 
 
+    // Program will run through the number of years that the user inputed, and then will run through the number of paychecks that the user inputs
     for (int i = 0; i < yearsToRun; i++){
       for(int x = 0; x < paychecksPerYear; x++){
+        // Exponentially increase the amount of the users investments. Add 1 to the users return on ivestment and add that to the fration ofration of 1/the users paycheck per year. Add this to the total.
         accountSize *= Math.pow(1+ returnOnInvestment, 1 / paychecksPerYear);
+
         accountSize += savingsPerPaycheck;
       }
     }
+    // Print out the users savings over the number of years they entered
     System.out.println("========================================");
     System.out.println("Your total savings over "+yearsToRun+" is "+accountSize+" Dollars");
     
